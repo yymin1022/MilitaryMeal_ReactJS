7192d89374a84720e2495b7c995db0402905599f
yymin1022 <yymin1022@gmail.com>
2021-05-25 10:18:32 +0000

    Update MealView.js : Removed TESTING

--- src/route/MealView/MealView.js
+++ src/route/MealView/MealView.js
@@ -4,12 +4,7 @@ class MealView extends Component{
   render(){
     return(
       <div className="MealView">
-        <div className="MealView-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="MealView-intro">
-          MealView Layout
-        </p>
+        MealView Layout
       </div>
     );
   }
