1be56e0f0d45e890122d7548e3bc06b5c25a9836
yymin1022 <yymin1022@gmail.com>
2021-05-26 10:41:30 +0000

    Update MealView.js : Add MenuItem Elements

--- src/route/MealView/MealView.js
+++ src/route/MealView/MealView.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 
-import 
+import MenuItem from "/home/server/ui/src/layout/MenuItem";
 
 class MealView extends Component{
   render(){
     return(
       <div className="MealView">
         MealView Layout
-
+        <MenuItem
+          menuType = "조식"
+          menuList = "맛김" />
+        <MenuItem
+          menuType = "중식"
+          menuList = "돼김볶" />
+        <MenuItem
+          menuType = "석식"
+          menuList = "햄소찌" />
       </div>
     );
   }
