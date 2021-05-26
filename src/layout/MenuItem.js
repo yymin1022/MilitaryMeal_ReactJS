b129eb7cfc9770ee26b4d10baf2552a6d5408eb0
yymin1022 <yymin1022@gmail.com>
2021-05-26 10:47:22 +0000

    Update MenuItem.js : Improve Text Header

--- src/layout/MenuItem.js
+++ src/layout/MenuItem.js
@@ -9,10 +9,8 @@ class MenuItem extends Component {
   render() {
     return(
       <div id="menuItem">
-        <p>Menu Item Component Start</p>
         <h4>{this.props.menuType}</h4>
         <p>{this.props.menuList}</p>
-        <p>Menu Item Component End</p>
       </div>
     )
   }
