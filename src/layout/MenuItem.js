5e662039e2a82e100b0ed4c23c3caafd620b7664
yymin1022 <yymin1022@gmail.com>
2021-05-26 09:51:18 +0000

    Add MenuItem Component

---
+++ src/layout/MenuItem.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class MenuItem extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div id="menuItem">
+        Menu Item Component
+      </div>
+    )
+  }
+}
+
+export default MenuItem;
