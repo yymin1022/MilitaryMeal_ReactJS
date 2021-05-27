c9ba880bade18a604e438a7be775172e8da2fede
yymin1022 <yymin1022@gmail.com>
2021-05-27 06:15:22 +0000

    Add RankItem.js

---
+++ src/layout/RankItem.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class RankItem extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div id="rankItem">
+        RankItem Component
+      </div>
+    )
+  }
+}
+
+export default RankItem;
