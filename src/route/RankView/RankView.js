949699a37ae9d861f85abd54812b1bf3b2edc40b
yymin1022 <yymin1022@gmail.com>
2021-05-27 06:16:03 +0000

    Update RankView.js : Add RankItem Component

--- src/route/RankView/RankView.js
+++ src/route/RankView/RankView.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 
+import RankItem from "/home/server/ui/src/layout/RankItem";
+
 class RankView extends Component{
   render(){
     return(
       <div className="RankView">
-        RankView Layout
+        <RankItem />
       </div>
     );
   }
