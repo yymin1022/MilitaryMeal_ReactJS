79a4148c80ceed27727d6d61d2517fd192b87fc6
yymin1022 <yymin1022@gmail.com>
2021-05-30 11:32:34 +0000

    Update MealDetail.js : Re-Structured Component

--- src/layout/MealDetail/MealDetail.js
+++ src/layout/MealDetail/MealDetail.js
@@ -1,47 +1,64 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import styled, {createGlobalStyle} from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-  html, body {
-    height: 100%
- }
-	body {
-		padding: 0;
-    margin: 0;
- }
-  #root {
-    height: 100%;
- }
-`;
+class MealDetail extends Component{
+    constructor() {
+        super();
+        this.state = {};
+    }
 
-const Container = styled.div`
-  padding-top: 100px;
-  padding-bottom: 150px;
-  padding-left: 50px;
-  padding-right: 50px;
+    UNSAFE_componentWillMount(){
+        this.menuDate = this.props.location.state.menuDate;
+        this.menuList = this.props.location.state.menuList;
+    
+        switch(this.props.location.state.menuType){
+          case 0:
+            this.menuType = "조식";
+            break;
+          case 1:
+            this.menuType = "중식";
+            break;
+          case 2:
+            this.menuType = "석식";
+            break;
+        }
+      }
 
-  height: 100%;
-`;
+    render(){
+        const GlobalStyle = createGlobalStyle`
+            html, body {
+                height: 100%
+            }
+            body {
+                padding: 0;
+                margin: 0;
+            }
+            #root {
+                height: 100%;
+            }
+            `;
+      
+        const Container = styled.div`
+            padding-top: 100px;
+            padding-bottom: 150px;
+            padding-left: 50px;
+            padding-right: 50px;
+        
+            height: 100%;
+            `;
+        
+        
 
-const MealDetail = props => {
-  const{
-    className,
-  } = props;
-  
-  return(
-    <Container className={className}>
-      <GlobalStyle />
-      <div>
-        MealDetail
-      </div>
-    </Container>
-  );
-}
-
-MealDetail.propTypes = {
-  className: PropTypes.string,
-}
+        return(
+            <Container className={className}>
+              <GlobalStyle />
+              <div>
+                MealDetail
+              </div>
+            </Container>
+        );
+    }
+  }
 
 export default MealDetail;
