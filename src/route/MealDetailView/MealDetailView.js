ae2f59136a4e70f746ed5c1b41b161be1c0568be
yymin1022 <yymin1022@gmail.com>
2021-05-30 10:20:38 +0000

    Add MealDetailView Route Component

---
+++ src/route/MealDetailView/MealDetailView.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class MealDetailView extends Component{
+  render(){
+    return(
+      <div className="MealDetailView">
+      </div>
+    );
+  }
+}
+
+export default MealDetailView;
