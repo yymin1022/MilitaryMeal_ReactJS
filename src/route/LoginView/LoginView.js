9069481670a29b45733e5027ba4fcbfef7be72a5
yymin1022 <yymin1022@gmail.com>
2021-05-31 07:49:37 +0000

    Add LoginView Component Files

---
+++ src/route/LoginView/LoginView.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class LoginView extends Component{
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <div className="MealView">
+        LoginView
+      </div>
+    );
+  }
+}
+
+export default LoginView;
