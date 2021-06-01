52a324e941929b29060fa83be0a912d4543c7d89
yymin1022 <yymin1022@gmail.com>
2021-06-01 04:32:42 +0000

    Revert "Add Checking for Session Undefined or not"
    This reverts commit 89465be836454185d34e9acd1844f9d4af0bf1cf.

--- src/route/MealView/MealView.js
+++ src/route/MealView/MealView.js
@@ -11,7 +11,7 @@ class MealView extends Component{
 
   UNSAFE_componentWillMount(){
     this.currentSession = "NO_LOGIN";
-    if(this.props.location.state.currentSession !== undefined){
+    if(this.props.location.state.currentSession != null){
       this.currentSession = this.props.location.state.currentSession;
     }
   }
