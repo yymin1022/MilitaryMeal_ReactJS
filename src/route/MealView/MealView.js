2d6056bc997cf063f86e067212997793fce0c029
yymin1022 <yymin1022@gmail.com>
2021-06-03 08:29:14 +0000

    Update MealView.js : Improve Structure

--- src/route/MealView/MealView.js
+++ src/route/MealView/MealView.js
@@ -19,45 +19,50 @@ class MealView extends Component{
     return(
       <div className="MealView">
         {/* <SessionCheck currentSession={this.currentSession}/> */}
-        <MenuItem
-          menuDate = "20210527"
-          menuList = {[
-            {name: "쌀밥",
-            calory: 100},
-            {name: "김치찌개",
-            calory: 100},
-            {name: "버섯볶음",
-            calory: 100},
-            {name: "오이무침",
-            calory: 100}
-          ]}
-          menuType = "0" />
-        <MenuItem
-          menuDate = "20210527"
-          menuList = {[
-            {name: "쌀밥",
-            calory: 100},
-            {name: "호박된장찌개",
-            calory: 100},
-            {name: "맛김",
-            calory: 100},
-            {name: "고등어순살조림",
-            calory: 100}
-          ]}
-          menuType = "1" />
-        <MenuItem
-          menuDate = "20210527"
-          menuList = {[
-            {name: "쌀밥",
-            calory: 100},
-            {name: "햄소시지찌개",
-            calory: 100},
-            {name: "닭고기양념튀김",
-            calory: 100},
-            {name: "배추김치",
-            calory: 100}
-          ]}
-          menuType = "2" />
+        <div className="mealDate">
+
+        </div>
+        <div className="mealItem">
+          <MenuItem
+            menuDate = "20210527"
+            menuList = {[
+              {name: "쌀밥",
+              calory: 100},
+              {name: "김치찌개",
+              calory: 100},
+              {name: "버섯볶음",
+              calory: 100},
+              {name: "오이무침",
+              calory: 100}
+            ]}
+            menuType = "0" />
+          <MenuItem
+            menuDate = "20210527"
+            menuList = {[
+              {name: "쌀밥",
+              calory: 100},
+              {name: "호박된장찌개",
+              calory: 100},
+              {name: "맛김",
+              calory: 100},
+              {name: "고등어순살조림",
+              calory: 100}
+            ]}
+            menuType = "1" />
+          <MenuItem
+            menuDate = "20210527"
+            menuList = {[
+              {name: "쌀밥",
+              calory: 100},
+              {name: "햄소시지찌개",
+              calory: 100},
+              {name: "닭고기양념튀김",
+              calory: 100},
+              {name: "배추김치",
+              calory: 100}
+            ]}
+            menuType = "2" />
+        </div>
       </div>
     );
   }
