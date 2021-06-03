58321c1fa5e56adfad9824eac574ff37367ba553
yymin1022 <yymin1022@gmail.com>
2021-06-03 06:04:10 +0000

    Update MealView.js : Update Menu Data Type - Add Calories Information

--- src/route/MealView/MealView.js
+++ src/route/MealView/MealView.js
@@ -19,15 +19,30 @@ class MealView extends Component{
         {/* <SessionCheck currentSession={this.currentSession}/> */}
         <MenuItem
           menuDate = "20210527"
-          menuList = "맛김"
+          menuList = {{
+            "쌀밥": 100,
+            "김치찌개": 100,
+            "버섯볶음": 100,
+            "오이무침": 100,
+            }}
           menuType = "0" />
         <MenuItem
           menuDate = "20210527"
-          menuList = "돼김볶"
+          menuList = {{
+            "쌀밥": 100,
+            "호박된장찌개": 100,
+            "고등어순살조림": 100,
+            "맛김": 100,
+            }}
           menuType = "1" />
         <MenuItem
           menuDate = "20210527"
-          menuList = "햄소찌"
+          menuList = {{
+            "쌀밥": 100,
+            "햄소시지찌개": 100,
+            "닭고기양념튀김": 100,
+            "배추김치": 100,
+            }}
           menuType = "2" />
       </div>
     );
