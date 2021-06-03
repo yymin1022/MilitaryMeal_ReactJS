53756f755ad5b3cd5ded88feac06669a678442dc
yymin1022 <yymin1022@gmail.com>
2021-06-03 02:23:48 +0000

    Update Default.js : Add CSS Importing

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -4,6 +4,8 @@ import {Link} from "react-router-dom";
 
 import {createGlobalStyle} from "styled-components";
 
+import "./Default.css";
+
 const GlobalStyle = createGlobalStyle`
   html, body {
     height: 100%
